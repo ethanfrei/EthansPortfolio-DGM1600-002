fad13c2e1bef632de32da0759897b50340acad14
Ethan Frei <ethanjfrei@gmail.com>
2021-10-26 13:31:59 -0600

    Star Wars commit buttons

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,12 +1,54 @@
 import { people } from '../Data/people.js'
+import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('#main')
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters.length)
-const femaleCharacters = people.filter(person => person.gender === 'female')
+const mainHeader = document.createElement('header')
+document.body.insertBefore(mainHeader, main)
 
-people.forEach((element) => {
+const allButton = document.createElement('button')
+allButton.textContent = 'All Characters'
+allButton.addEventListener('click', () => populateDOM(people))
+mainHeader.appendChild(allButton)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+mainHeader.appendChild(femaleButton)
+
+
+const othersButton = document.createElement('button')
+othersButton.textContent = 'Other Characters'
+othersButton.addEventListener('click', () => populateDOM(otherCharacters))
+mainHeader.appendChild(othersButton)
+
+const maleCharacters = people.filter((person) => person.gender === 'male')
+
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+
+const otherCharacters = people.filter((person) => {
+
+if (
+    person.gender === 'n/a' ||
+    person.gender === 'hermaphrodite' ||
+    person.gender === 'none'
+) {
+    return person
+}
+})
+
+function populateDOM(characters) {
+
+while (main.firstChild) {
+    main.removeChild(main.firstChild)
+}
+
+characters.forEach((element) => {
 const personFig = document.createElement('figure')
 const personImg = document.createElement('img')
 let charNum = getLastNumber(element.url)
@@ -21,13 +63,4 @@ main.appendChild(personFig)
 
 }
 )
-
-
-function getLastNumber(url){
-let end = url.lastIndexOf('/')
-let start = end - 2
-if (url.charAt(start) === '/') {
-    start++
-}
-return url.slice(start, end)
 }
--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,16 +1,20 @@
 import { films } from '../Data/films.js'
+import { getLastNumber } from '../utils/index.js'
 
 let filmList = document.querySelector('#filmList')
 
 for (let i = 0; i < films.length; i++) {
     let figure = document.createElement('figure')
-let figImg = document.createElement('img')
-figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
-let figCaption = document.createElement('figcaption')
-figCaption.textContent = films[i].title
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
 
-figure.appendChild(figImg)
-figure.appendChild(figCaption)
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
 
-filmList.appendChild(figure)
+    figCaption.textContent = foundFilm.title
+
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+
+    filmList.appendChild(figure)
 }
