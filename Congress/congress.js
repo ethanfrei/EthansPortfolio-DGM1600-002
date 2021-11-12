b585ffb1980db7fd740ccb84fbc63b43cb8df7a5
Ethan Frei <ethanjfrei@gmail.com>
2021-11-12 11:04:48 -0700

    Senators Oush 11-12

--- Congress/congress.js
+++ Congress/congress.js
@@ -1,18 +1,35 @@
-import { senators } from '../Data/senators'
+import { senators } from '../Data/senators.js'
 
-const senatorDiv = document.querySelector('.senators')
+const senatorDiv = document.querySelector('.Senators')
 
-function populateSenatorDiv() {
-    senators.forEach(senator => {
+function simplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ``
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            party: senator.party,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+        }
+    })
+}
+
+
+
+populateSenatorDiv(simplifiedSenators(senators))
+
+function populateSenatorDiv(simpleSenators) {
+    simpleSenators.forEach(senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
 
-        figCaption.textContent = senator.first_name
-        senatorFigure.appendChild(figImg)
+figImg.src = senator.imgURL
+
+        figCaption.textContent = senator.name
+        senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorDiv.appendChild(senFigure)
     })
 }
 
-populateSenatorDiv
