d3040c970fde8da6c7a82adc02795b9986ef6591
Ethan Frei <ethanjfrei@gmail.com>
2021-11-16 10:55:50 -0700

    Update with Congress

--- Congress/congress.js
+++ Congress/congress.js
@@ -2,8 +2,8 @@ import { senators } from '../Data/senators.js'
 
 const senatorDiv = document.querySelector('.Senators')
 
-function simplifiedSenators(senatorArray) {
-    return senatorArray.map(senator => {
+function simplifiedSenators() {
+    return senators.map(senator => {
         const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ``
         return {
             id: senator.id,
@@ -11,7 +11,7 @@ function simplifiedSenators(senatorArray) {
             party: senator.party,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
             gender: senator.gender,
-            seniority: "later",
+            seniority: +senator.seniority,
             missedVotesPct: senator.missed_votes_pct,
             loyaltyPct: senator.votes_with_party_pct,
         }
@@ -20,7 +20,7 @@ function simplifiedSenators(senatorArray) {
 
 
 
-populateSenatorDiv(simplifiedSenators(senators))
+populateSenatorDiv(simplifiedSenators())
 
 function populateSenatorDiv(simpleSenators) {
     simpleSenators.forEach(senator => {
@@ -37,12 +37,17 @@ figImg.src = senator.imgURL
     })
 }
 
-const filterSenators = (prop, value) => {
-return simplifiedSenators(senators).filter(senator => senator[prop] === value)
-}
+const filterSenators = (prop, value) => simplifiedSenators().filter(senator => senator[prop] === value)
+
 
 const republicans = filterSenators('party','R')
 const femaleSenators = filterSenators('gender', 'F')
     
-console.log(republicans)
+//console.log(republicans, femaleSenators)
+
+const mostSeniorSenator = simplifiedSenators().reduce((acc, senator)=> {
+    return acc.seniority > senator.seniority ? acc : senator
+})
+
+console.log(mostSeniorSenator)
 
